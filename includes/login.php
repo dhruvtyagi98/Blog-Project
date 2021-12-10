39b17e0423ae974cd8299502d39d6e5884016539
Dhruv <dhruv.vdoit@gmail.com>
2021-12-10 14:34:06 +0530

    feat: login and registration validations both backend and frontend along with logout functionality and notification messages.

--- includes/login.php
+++ includes/login.php
@@ -1,5 +1,7 @@
 <?php 
 
+session_start();
+
 // checking if the user came from form subit or not.
 if (isset($_POST['login_button'])) {
     
@@ -11,16 +13,22 @@ if (isset($_POST['login_button'])) {
 
     //checking if the user submitted all the required fields or not.
     if (empty($email) || empty($password)) {
-        $_SESSION['login_error']['empty'] = 'Empty Fields!';
+        $_SESSION['login_error'] = 'Empty Fields!';
+        $connection->close();
+        header("Location: ../index.php");
+    }
+    elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $_SESSION['invalid_email_login'] = 'Invalid Email!';
         $connection->close();
         header("Location: ../index.php");
     }
     else{
 
+        //checking if the email is present or not.
         $query_email = "SELECT * FROM users WHERE email = '$email'";
         $result = $connection->query($query_email);
         if ($result->num_rows == 0) {
-            $_SESSION['email_error'] = 'Email Not Found!';
+            $_SESSION['email_not_found'] = 'Email Not Found!';
             $connection->close();
             header("Location: ../index.php");
         }
@@ -30,6 +38,11 @@ if (isset($_POST['login_button'])) {
             if (password_verify($password, $user['password']) == true) {
                 $_SESSION['user_id'] = $user['id'];
                 $_SESSION['username'] = $user['name'];
+                $connection->close();
+                header("Location: ../index.php");
+            }
+            else{
+                $_SESSION['invalid_password'] = 'Invalid Password';
                 $connection->close();
                 header("Location: ../index.php");
             }
--- navbar.php
+++ navbar.php
@@ -4,35 +4,35 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="float-right" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Users</a>
-                </li>
-                <?php 
-                    if (!isset($_SESSION['name'])) {
-                        echo '<li class="nav-item">
-                                <button class="btn btn-primary" type="button" style="border-radius: 25px;padding: 10px;padding-inline: 30px;" data-bs-target="#login_modal" data-bs-toggle="modal">Login</button>
-                            </li>';
-                    }
-                    else{
-                        echo '<li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Welcome, User
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Profile</a></li>
-                                <li><hr class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="#">Logout</a></li>
-                            </ul>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Users</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav">
+            <?php 
+                if (!isset($_SESSION['username'])) {
+                    echo '<li class="nav-item">
+                            <button class="btn btn-primary" type="button" style="border-radius: 25px;padding: 10px;padding-inline: 30px;" data-bs-target="#login_modal" data-bs-toggle="modal">Login</button>
                         </li>';
-                    }
-                ?>
-            </ul>
-        </div>
+                }
+                else{
+                    echo '<li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Welcome, '. $_SESSION['username'] .'
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Profile</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="includes/logout.php">Logout</a></li>
+                        </ul>
+                    </li>';
+                }
+            ?>
+        </ul>
     </div>
 </nav>
 
@@ -47,13 +47,25 @@
                 </div>
                 <form action="includes/login.php" method="post">
                     <div class="modal-body">
+                        <div class="error empty_fields_login">
+                            <?php if (isset($_SESSION['login_error'])) echo $_SESSION['login_error'] ?>
+                        </div>
                         <div class="mb-4">
                             <label for="login_email">Email &nbsp;<span class="text-danger">*</span></label>
                             <input type="email" class="form-control" id="login_email" name="email" placeholder="Enter Email">
+                            <div class="error email_not_found">
+                                <?php if (isset($_SESSION['email_not_found'])) echo $_SESSION['email_not_found'] ?>
+                            </div>
+                            <div class="error invalid_email_login">
+                                <?php if (isset($_SESSION['invalid_email_login'])) echo $_SESSION['invalid_email_login'] ?>
+                            </div>
                         </div>
                         <div class="mb-4">
                             <label for="login_password">Password &nbsp;<span class="text-danger">*</span></label>
                             <input type="password" class="form-control" id="login_password" name="password" placeholder="Enter Password">
+                            <div class="error invalid_password">
+                                <?php if (isset($_SESSION['invalid_password'])) echo $_SESSION['invalid_password'] ?>
+                            </div>
                         </div>
                         <div>
                             <p>Not Registered? <a type="button" id="register" style="color: blue;">Register</a></p>
@@ -65,24 +77,40 @@
                     </div>
                 </form>
             </div>
-            <div id="register_div">
+            <div id="register_div" style="display: none;">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Register</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <form action="includes/register.php" method="post">
                     <div class="modal-body">
+                        <div class="error empty_fields_register">
+                            <?php if (isset($_SESSION['register_error'])) echo $_SESSION['register_error'] ?>
+                        </div>
                         <div class="mb-4">
                             <label for="name">Name &nbsp;<span class="text-danger">*</span></label>
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Enter Name" required>
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Enter Name">
                         </div>
                         <div class="mb-4">
                             <label for="register_email">Email &nbsp;<span class="text-danger">*</span></label>
-                            <input type="email" class="form-control" id="reqister_email" name="email" placeholder="Enter Email" required>
+                            <input type="email" class="form-control" id="reqister_email" name="email" placeholder="Enter Email">
+                            <div class="error invalid_email">
+                                <?php if (isset($_SESSION['invalid_email'])) echo $_SESSION['invalid_email']?>
+                            </div>
+                            <div class="error email_error">
+                                <?php if (isset($_SESSION['email_error'])) echo $_SESSION['email_error']?>
+                            </div>
                         </div>
                         <div class="mb-4">
                             <label for="register_password">Password &nbsp;<span class="text-danger">*</span></label>
-                            <input type="password" class="form-control" id="register_password" name="password" placeholder="Enter Password" required minlength="6">
+                            <input type="password" class="form-control" id="register_password" name="password" placeholder="Enter Password">
+                        </div>
+                        <div class="mb-4">
+                            <label for="register_password_confirm">Confirm Password &nbsp;<span class="text-danger">*</span></label>
+                            <input type="password" class="form-control" id="register_password_confirm" name="password_confirm" placeholder="Confirm Password">
+                            <div class="error password_mismatch">
+                                <?php if (isset($_SESSION['password_mismatch'])) echo $_SESSION['password_mismatch']?>
+                            </div>
                         </div>
                         <div>
                             <p>Already Registered? <a type="button" id="login" style="color: blue;">Login</a></p>
@@ -100,7 +128,6 @@
 
 <script>
     $(document).ready(function() {
-        $('#register_div').hide();
 
         $('#register').on('click', function(){
             $('#register_div').show();
@@ -111,5 +138,58 @@
             $('#register_div').hide();
             $('#login_div').show();
         })
+
+        if ('<?php echo isset($_SESSION['invalid_email']) ?>' == 1) {
+            $('#login_modal').modal('show');
+            $('#register_div').show();
+            $('#login_div').hide();
+            $('.invalid_email').show();
+            '<?php unset($_SESSION['invalid_email']) ?>'
+        }
+        else if ('<?php echo isset($_SESSION['register_error']) ?>' == 1) {
+            $('#login_modal').modal('show');
+            $('#register_div').show();
+            $('#login_div').hide();
+            $('.empty_fields_register').show();
+            '<?php unset($_SESSION['register_error']) ?>'
+        }
+        else if ('<?php echo isset($_SESSION['password_mismatch']) ?>' == 1) {
+            $('#login_modal').modal('show');
+            $('#register_div').show();
+            $('#login_div').hide();
+            $('.password_mismatch').show();
+            '<?php unset($_SESSION['password_mismatch']) ?>'
+        }
+        else if ('<?php echo isset($_SESSION['email_error']) ?>' == 1) {
+            $('#login_modal').modal('show');
+            $('#register_div').show();
+            $('#login_div').hide();
+            $('.email_error').show();
+            '<?php unset($_SESSION['email_error']) ?>'
+        }
+        else if ('<?php echo isset($_SESSION['email_not_found']) ?>' == 1) {
+            $('#login_modal').modal('show');
+            $('#login_div').show();
+            $('.email_not_found').show();
+            '<?php unset($_SESSION['email_not_found']) ?>'
+        }
+        else if ('<?php echo isset($_SESSION['login_error']) ?>' == 1) {
+            $('#login_modal').modal('show');
+            $('#login_div').show();
+            $('.empty_fields_login').show();
+            '<?php unset($_SESSION['login_error']) ?>'
+        }
+        else if ('<?php echo isset($_SESSION['invalid_email_login']) ?>' == 1) {
+            $('#login_modal').modal('show');
+            $('#login_div').show();
+            $('.invalid_email_login').show();
+            '<?php unset($_SESSION['invalid_email_login']) ?>'
+        }
+        else if ('<?php echo isset($_SESSION['invalid_password']) ?>' == 1) {
+            $('#login_modal').modal('show');
+            $('#login_div').show();
+            $('.invalid_password').show();
+            '<?php unset($_SESSION['invalid_password']) ?>'
+        }
     });
 </script>
