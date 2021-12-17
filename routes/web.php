864c5e7f8cf5eee383298ed8d3e3f750f08b838c
Dhruv <dhruv.vdoit@gmail.com>
2021-12-17 18:02:43 +0530

    removed unnecessary routes from web.php.

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,11 @@ $check_password     = "includes/auth/check_password.php";
 // Blog Routes
 $blog_controller    = "includes/blog/blog_controller.php";
 $comment_controller = "includes/blog/comment_controller.php";
-$delete_blog        = "includes/blog/delete_blog.php";
 $get_all_blogs      = "includes/blog/get_all_blogs.php";
 $get_blog           = "includes/blog/get_blog.php";
 $get_comments       = "includes/blog/get_comments.php";
 $get_update_blog    = "includes/blog/get_update_blog.php";
 $get_user_blogs     = "includes/blog/get_user_blogs.php";
-$update_blog        = "includes/blog/update_blog.php";
 
 // User Routes
 $get_users          = "includes/user/get_users.php";
